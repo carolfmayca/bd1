70b95ee1ae6272879a94d5260066d7832e94b71b
carolfmayca <carolfmayca@gmail.com>
2025-09-21 18:52:39 -0400

    agr vai amem

--- src/tp1_3.2.py
+++ src/tp1_3.2.py
@@ -25,36 +25,16 @@ def find_next_line(iterator, text):
             return line
     return None
 
-def insert_products(conn, produto):
-    """
-    Insere um produto na tabela Products.
-    """
-    if produto['title'] is None:
-        return
-
+def batch_insert_products(conn, data):
+    """Insere uma lista de produtos em lote."""
     sql = """
         INSERT INTO Products (Id, ASIN, title, "group", salesrank, numSimilar, numCategories, numReviews, downloaded, avg_rating)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         ON CONFLICT (ASIN) DO NOTHING;
-        """
+    """
     with conn.cursor() as cur:
+        execute_batch(cur, sql, data)
 
-        num_similar = 0
-        if produto['similar'] is not None:
-            num_similar = produto['similar'].split()[0]
-
-        cur.execute(sql, (
-            produto['id'],
-            produto['asin'],
-            produto['title'],
-            produto['group'],
-            produto['salesrank'],
-            num_similar, 
-            produto['numCategories'],
-            produto['numReviews'],
-            produto['downloaded'],
-            produto['avg_rating']
-        ))
 
 
 def find_id(categorie):
@@ -70,27 +50,22 @@ def find_id(categorie):
         return match
     return None
 
-def insert_general_categories(conn,categories):
-    categorie = find_id(categories)
-    if categorie is not None:
-        sql = """
-            INSERT INTO Categories (id_categorie, categorie_name, id_father)
-            VALUES (%s, %s, %s)
-            ON CONFLICT (id_categorie) DO NOTHING;
-            """
-        with conn.cursor() as cur:
-            cat_name = categorie[0][0] if categorie[0][0] != "" else None
-            cur.execute(sql, (categorie[0][1], cat_name, None))
-            for i in range(1,len(categorie)):
-                cat_name = categorie[i][0] if categorie[i][0] != "" else None
-                cur.execute(sql, (categorie[i][1], cat_name, categorie[i-1][1]))
+def batch_insert_general_categories(conn, data):
+    """Insere uma lista de categorias gerais em lote."""
+    sql = """
+        INSERT INTO Categories (id_categorie, categorie_name, id_father)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (id_categorie) DO NOTHING;
+    """
+    with conn.cursor() as cur:
+        execute_batch(cur, sql, data)
 
 def batch_insert_reviews(conn, data):
     """Insere uma lista de reviews em lote."""
     sql = """
         INSERT INTO Reviews (ASIN, "date", customer, rating, votes, helpful)
         VALUES (%s, %s, %s, %s, %s, %s)
-        ON CONFLICT (id) DO NOTHING;
+        ON CONFLICT DO NOTHING;
     """
     with conn.cursor() as cur:
         execute_batch(cur, sql, data)
@@ -121,88 +96,89 @@ def insert_data(conn, input_file):
     """
     print("Iniciando o parsing do arquivo...")
     with open(input_file, 'r') as f:
-        arquivo = (line for line in f.read().splitlines() if line.strip())
-        produtos = []
         general_categories = set()
-        for line in arquivo:
+        produtos_data = []
+        reviews = []
+        product_categories_data = set()
+        valid_asins = set()
+        similars = []
+        for line in f:
             if line.startswith('Id'):
                 id_ = line.split(':')[1].strip()
-                line = find_next_line(arquivo, 'ASIN')
+                line = find_next_line(f, 'ASIN')
                 asin = line.split(':')[1].strip()
-                line = next(arquivo)
+                line = next(f)
                 title = group = salesrank = similar = None
-                categories = reviews = num_categories = num_reviews = downloaded = avg_rating = None
+                num_categories = num_reviews = downloaded = avg_rating = 0
                 if not "discontinued product" in line:
                     title = ":".join(line.split(':')[1:]).strip()
-                    line = find_next_line(arquivo, 'group')
+                    line = find_next_line(f, 'group')
                     group = line.split(':')[1].strip()
-                    line = find_next_line(arquivo, 'salesrank')
+                    line = find_next_line(f, 'salesrank')
                     salesrank = line.split(':')[1].strip()
-                    line = find_next_line(arquivo, 'similar')
+                    line = find_next_line(f, 'similar')
                     similar = line.split(':')[1].strip()
-                    line = find_next_line(arquivo, 'categories')
+                    similars.append((asin, similar[1:]) if similar and similar.split()[0] != '0' else ())
+                    line = find_next_line(f, 'categories')
                     num_categories = line.split(':')[1].strip()
-                    categories = []
                     for _ in range(int(num_categories)):
-                        line = next(arquivo)
+                        line = next(f)
                         general_categories.add(line)
-                        categories.append([line.strip()])
-                    line = find_next_line(arquivo, 'reviews')
+                        categories_id = find_id(line.strip())
+                        if categories_id:
+                            for cat in categories_id:
+                                product_categories_data.add((asin, cat[1]))
+                    line = find_next_line(f, 'reviews')
                     line_reviews = line.split(':')
-                    num_reviews = line_reviews[2][1].strip()
-                    downloaded = line_reviews[3][1].strip()
-                    avg_rating = line_reviews[4][1].strip()
-                    reviews = []
+                    num_reviews = line_reviews[2].split()[0]
+                    downloaded = line_reviews[3].split()[0]
+                    avg_rating = line_reviews[4].split()[0]
                     for _ in range(int(downloaded)):
-                        line = next(arquivo)
-                        reviews.append([line.strip()])
-                produtos.append({'id':id_, 'asin':asin, 'title':title, 'group':group, \
-                                'salesrank':salesrank,'similar':similar, \
-                                'numCategories':num_categories, 'categories': categories,\
-                                 'numReviews':num_reviews, 'reviews':reviews, \
-                                 'downloaded':downloaded, 'avg_rating':avg_rating})
-    print(f"Parsing concluído. {len(produtos)} produtos encontrados.")
+                        line = next(f)
+                        parts = ' '.join(line.split()).split()
+                        if len(parts) == 9:
+                            reviews.append((asin, parts[0], parts[2], parts[4], parts[6], parts[8]))
+
+                produtos_data.append((id_, asin, title, group, salesrank, similar[0] if similar else None, num_categories, num_reviews, downloaded, avg_rating))
+                valid_asins.add(asin)
+    print(f"Parsing concluído. {len(produtos_data)} produtos encontrados.")
     print("Inserindo categorias gerais e produtos...")
+    
+    batch_insert_products(conn, produtos_data)
+    general_categories_data = []
+    processed_cat_ids = set()
     for i in general_categories:
-        insert_general_categories(conn,i)
-    valid_asins = set()
-    for i in produtos:
-        if i['title'] is not None:
-            insert_products(conn,i)
-            valid_asins.add(i['asin'])
+        categorie = find_id(i)
+        if categorie:
+            cat_name = categorie[0][0] if categorie[0][0] != "" else None
+            if categorie[0][1] not in processed_cat_ids:
+                general_categories_data.append((categorie[0][1], cat_name, None))
+                processed_cat_ids.add(categorie[0][1])
+            for i in range(1, len(categorie)):
+                cat_name = categorie[i][0] if categorie[i][0] != "" else None
+                if categorie[i][1] not in processed_cat_ids:
+                    general_categories_data.append((categorie[i][1], cat_name, categorie[i-1][1]))
+                    processed_cat_ids.add(categorie[i][1])
+    batch_insert_general_categories(conn, general_categories_data)
     
     print("Produtos e categorias gerais inseridos.")
-    print("Inserindo avaliações, categorias específicas e produtos similares...")
+    print("Continuando inserções...")
     print("\033[37mwarning: aqui demora um pouco, recomendo ir tomar um café ☕\033[0m")
     print("\033[37mtempo médio: 8 minutos (obviamente depende do pc)\033[0m")
-    reviews_data = []
-    product_categories_data = []
     similars_data = []
-    for i in produtos:
-        if i['title'] is None:
-            continue
-        if i['reviews'] is not None:
-            for j in i['reviews']:
-                parts = ' '.join(j[0].split()).split()
-                reviews_data.append((i['asin'], parts[0], parts[2], parts[4], parts[6], parts[8]))
-        if i['categories'] is not None:
-            for j in i['categories']:
-                categories_id = find_id(j)
-                if categories_id:
-                    for cat in categories_id:
-                        product_categories_data.append((i['asin'], cat[1]))
-        if i['similar'] is not None and i['similar'].split()[0] != '0':
-            similar_parts = i['similar'].split()
-            for j in similar_parts[1:]:
+    for i in similars:
+        if len(i) == 2 and i[1]:
+            similar_parts = i[1].split()
+            print(i[0])
+            for j in similar_parts:
                 if j in valid_asins:
-                    similars_data.append((i['asin'], j))
-    batch_insert_reviews(conn, reviews_data)
+                    similars_data.append((i[0], j))
+    batch_insert_reviews(conn, reviews)
     batch_insert_product_categories(conn, product_categories_data)
-    batch_insert_similars(conn, similars_data)         
+    batch_insert_similars(conn, similars_data)
     print("Dados inseridos com sucesso.")
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="Script para popular o banco de dados de e-commerce.")
     parser.add_argument('--db-host', default=os.environ.get('PGHOST', 'db'), help='Host do banco de dados')
     parser.add_argument('--db-port', default=os.environ.get('PGPORT', 5432), help='Porta do banco de dados')
@@ -231,7 +207,7 @@ if __name__ == "__main__":
         print("Ocorreu um erro. Revertendo operações.\nErro:", error)
         if conn:
             conn.rollback()
-
+    
     finally:
         if conn:
             conn.close()
