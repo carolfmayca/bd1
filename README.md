5e4facb6690a9df3ac44257d55d4f74f2b3c1058
FernandooLAN <fernando.nascimento@icomp.ufam.edu.br>
2025-09-24 12:05:29 -0400

    acho que terminamos

---
+++ README.md
@@ -0,0 +1,31 @@
+## Trabalho Prático I - Bancos de Dados (2025/02)
+Para executar o projeto, siga os passos abaixo. É necessário ter o Docker e o Docker Compose instalados.
+### Construir e Iniciar os Serviços
+Este comando irá construir a imagem da aplicação Python e iniciar os contêineres do banco de dados e da aplicação em segundo plano.
+
+```docker compose up -d --build```
+
+
+### (Opcional) Verificar a Saúde do Banco de Dados
+Você pode usar este comando para garantir que o contêiner do PostgreSQL (db) está rodando e saudável.
+```docker compose ps```
+
+
+### Criar o Esquema e Carregar os Dados
+Este comando executa o script tp1_3.2.py, que irá criar as tabelas e popular o banco de dados com os dados do arquivo snap_amazon.txt. Este processo pode levar alguns minutos.
+
+```docker compose run --rm app python src/tp1_3.2.py \ --db-host db --db-port 5432 --db-name ecommerce --db-user postgres --db-pass postgres \ --input /data/snap_amazon.txt ```
+
+
+### Executar o Dashboard
+Este comando executa o script tp1_3.3.py, que irá rodar todas as 7 consultas do dashboard. Os resultados serão exibidos no terminal e também salvos em arquivos CSV na pasta out/.
+Para rodar com o ASIN padrão:
+
+```docker compose run --rm app python src/tp1_3.3.py --db-host db --db-port 5432 --db-name ecommerce --db-user postgres --db-pass postgres --output /app/out  ```
+
+
+- Para rodar com um ASIN específico (opcional):
+Para as consultas 1, 2 e 3, você pode especificar um produto diferente usando o argumento --product-asin.
+```docker compose run --rm app python src/tp1_3.3.py --db-host db --db-port 5432 --db-name ecommerce --db-user postgres --db-pass postgres --output /app/out --product-asin 0801983258```
+
+
--- src/tp1_3.3.py
+++ src/tp1_3.3.py
@@ -8,226 +8,199 @@ import pandas as pd
 # Configuração básica do logging para exibir mensagens no terminal
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def query_1(cursor, product_asin):
+def query_1(cursor, product_asin, output_dir):
     """
-    Dado um produto, listar os 5 comentários mais úteis e com maior avaliação 
-    e os 5 comentários mais úteis e com menor avaliação.
+    Lista os 5 comentários com maior e menor avaliação, salvando em CSV.
     """
     if not product_asin:
-        logging.warning("Nenhum ASIN de produto fornecido para a Query 1. Pulando.")
         return
-
     logging.info(f"Executando Query 1 para o ASIN: {product_asin}")
     
-    sql_top_rated = """
-        SELECT asin, rating, helpful, votes, customer FROM reviews 
-        WHERE asin = %s
-        ORDER BY rating DESC, helpful DESC 
-        LIMIT 5;
-    """ 
-    cursor.execute(sql_top_rated, (product_asin,))
+    # Maior Avaliação
+    sql_top = """
+        SELECT asin, rating, helpful, votes, customer 
+        FROM reviews 
+        WHERE asin = %s ORDER BY rating DESC, helpful DESC LIMIT 5;
+    """
+
+    cursor.execute(sql_top, (product_asin,))
     results_top = cursor.fetchall()
-    
-    print("\n--- Query 1: Top 5 Comentários (Mais Úteis, Maior Avaliação) ---")
+    print("\n--- Query 1: Top 5 Comentários (Maior Avaliação, Mais Úteis) ---")
+
     if results_top:
         df_top = pd.DataFrame(results_top, columns=[desc[0] for desc in cursor.description])
         print(df_top)
-    else:
-        print(f"Nenhum comentário encontrado para o produto com ASIN {product_asin}.")
+        file_path = os.path.join(output_dir, f"q1_maior_avaliacao_{product_asin}.csv")
+        df_top.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-    sql_low_rated = """
-        SELECT asin, rating, helpful, votes, customer FROM reviews 
-        WHERE asin = %s
-        ORDER BY helpful DESC, rating ASC 
-        LIMIT 5;
+    # Menor Avaliação
+    sql_low = """
+        SELECT asin, rating, helpful, votes, customer 
+        FROM reviews 
+        WHERE asin = %s ORDER BY rating ASC, helpful DESC LIMIT 5;
     """
-    cursor.execute(sql_low_rated, (product_asin,))
-    results_low = cursor.fetchall()
 
-    print("\n--- Query 1: Top 5 Comentários (Mais Úteis, Menor Avaliação) ---")
+    cursor.execute(sql_low, (product_asin,))
+    results_low = cursor.fetchall()
+    print("\n--- Query 1: Top 5 Comentários (Menor Avaliação, Mais Úteis) ---")
     if results_low:
         df_low = pd.DataFrame(results_low, columns=[desc[0] for desc in cursor.description])
         print(df_low)
-    else:
-        print(f"Nenhum comentário encontrado para o produto com ASIN {product_asin}.")
+        file_path = os.path.join(output_dir, f"q1_menor_avaliacao_{product_asin}.csv")
+        df_low.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-def query_2(cursor, product_asin):
-    """
-    Dado um produto, listar os produtos similares com maiores vendas (melhor salesrank) do que ele.
-    """
+def query_2(cursor, product_asin, output_dir):
     if not product_asin:
-        logging.warning("Nenhum ASIN de produto fornecido para a Query 2. Pulando.")
         return
-
     logging.info(f"Executando Query 2 para o ASIN: {product_asin}")
-    
+
     sql = """
-        SELECT
-            p2.asin,
-            p2.title,
-            p2.salesrank
-        FROM
-            products AS p1
-        JOIN
-            "Similar" AS s ON p1.asin = s.asin_product
-        JOIN
-            products AS p2 ON s.asin_similar = p2.asin
-        WHERE
-            p1.asin = %s AND p2.salesrank < p1.salesrank
-        ORDER BY
-            p2.salesrank ASC;
+        SELECT p2.asin, p2.title, p2.salesrank 
+        FROM products AS p1
+        JOIN "Similar" AS s ON p1.asin = s.asin_product
+        JOIN products AS p2 ON s.asin_similar = p2.asin
+        WHERE p1.asin = %s AND p2.salesrank < p1.salesrank AND p2.salesrank > 0
+        ORDER BY p2.salesrank ASC;
     """
+
     cursor.execute(sql, (product_asin,))
     results = cursor.fetchall()
-    
     print("\n--- Query 2: Produtos Similares com Melhor Salesrank ---")
     if results:
         df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
         print(df)
-    else:
-        print(f"Nenhum produto similar com melhor salesrank encontrado para o ASIN {product_asin}.")
+        file_path = os.path.join(output_dir, f"q2_similares_melhor_venda_{product_asin}.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-
-# def query_3(cursor, product_asin):
-    """
-    Dado um produto, mostrar a evolução diária das médias de avaliação ao longo do período coberto no arquivo.
-    """
+def query_3(cursor, product_asin, output_dir):
     if not product_asin:
-        logging.warning("Nenhum ASIN de produto fornecido para a Query 3. Pulando.")
         return
-
-    logging.info(f"Executando Query 2 para o ASIN: {product_asin}")
+    logging.info(f"Executando Query 3 para o ASIN: {product_asin}")
 
     sql = """
-        SELECT
-            DATE(review_date) AS review_date,
-            AVG(rating) AS average_rating
-        FROM
-            reviews
-        WHERE
-            asin = %s
-        GROUP BY
-            DATE(review_date)
-        ORDER BY
-            DATE(review_date);
+        WITH daily_avg AS (
+            SELECT "date", AVG(rating) AS avg_rating 
+            FROM reviews
+            WHERE asin = %(asin)s GROUP BY "date"
+        ), date_series AS (
+            SELECT generate_series(
+                (SELECT MIN("date") FROM reviews WHERE asin = %(asin)s),
+                (SELECT MAX("date") FROM reviews WHERE asin = %(asin)s),
+                '1 day'::interval
+            )::date AS dia
+        )
+        SELECT ds.dia, COALESCE(CAST(da.avg_rating AS NUMERIC(10, 2)), 0) AS media_diaria
+        FROM date_series ds 
+        LEFT JOIN daily_avg da ON ds.dia = da."date"
+        ORDER BY ds.dia ASC;
     """
-    cursor.execute(sql, (product_asin,))
-    results = cursor.fetchall()
 
-    print("\n--- Query 3: Evolução Diária das Médias de Avaliação ---")
+    cursor.execute(sql, {'asin': product_asin})
+    results = cursor.fetchall()
+    print("\n--- Query 3: Evolução Diária da Média de Avaliação ---")
     if results:
         df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
         print(df)
-    else:
-        print(f"Nenhuma avaliação encontrada para o produto com ASIN {product_asin}.")
+        file_path = os.path.join(output_dir, f"q3_evolucao_diaria_{product_asin}.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-def query_4(cursor):
-    """
-    Listar os 10 produtos líderes de venda em cada grupo de produtos.
-    """
-    logging.info("Executando Query 4 para listar os 10 produtos líderes de venda em cada grupo de produtos.")
-    
+def query_4(cursor, output_dir):
+    logging.info("Executando Query 4...")
 
     sql = """
         WITH ranked_products AS (
-            SELECT
-                title,
-                asin,
-                "group",
-                salesrank,
-                ROW_NUMBER() OVER (PARTITION BY "group" ORDER BY salesrank DESC) AS rn
-            FROM
-                products
-            WHERE
-                salesrank >= 0
+            SELECT title, asin, "group", salesrank,
+                   ROW_NUMBER() OVER (PARTITION BY "group" ORDER BY salesrank ASC) AS rn
+            FROM products WHERE salesrank > 0
         )
-        SELECT
-            *
-        FROM
-            ranked_products
-        WHERE
-            rn <= 10;
+        SELECT title, asin, "group", salesrank 
+        FROM ranked_products 
+        WHERE rn <= 10;
     """
+
     cursor.execute(sql)
     results = cursor.fetchall()
-    
     print("\n--- Query 4: Top 10 Produtos Líderes de Venda por Grupo ---")
     if results:
-        # A filtragem agora é feita no SQL, então o Dataframe já vem pronto.
         df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
         print(df)
-    else:
-        print("Nenhum produto encontrado.")
+        file_path = os.path.join(output_dir, "q4_top10_produtos_por_grupo.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-def query_5(cursor):
-    """
-    Listar os 10 produtos com a maior média de avaliações úteis positivas por produto.
-    """
-    logging.info("Executando Query 5 para listar os 10 produtos com a maior média de avaliações úteis positivas por produto.")
+def query_5(cursor, output_dir):
+    logging.info("Executando Query 5...")
 
     sql = """
-        SELECT
-            p.asin,
-            p.title,
-            CAST(AVG(r.helpful) AS NUMERIC(10, 2)) AS media_avaliacoes_uteis        
-        FROM
-            reviews AS r
-        JOIN
-            products AS p ON r.asin = p.asin
-        GROUP BY
-            p.asin, p.title
-        ORDER BY
-            media_avaliacoes_uteis DESC
-        LIMIT 10;
+        SELECT p.asin, p.title, AVG(r.helpful)::NUMERIC(10, 2) AS media_avaliacoes_uteis
+        FROM reviews AS r JOIN products AS p ON r.asin = p.asin
+        WHERE r.rating > 3
+        GROUP BY p.asin, p.title ORDER BY media_avaliacoes_uteis DESC LIMIT 10;
     """
+
     cursor.execute(sql)
     results = cursor.fetchall()
-
-    print("\n--- Query 5: Top 10 Produtos com Maior Média de Avaliações Úteis Positivas ---\n")
+    print("\n--- Query 5: Top 10 Produtos com Maior Média de Avaliações Úteis Positivas ---")
     if results:
         df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
         print(df)
-    else:
-        print("Nenhum produto encontrado.")
+        file_path = os.path.join(output_dir, "q5_top10_produtos_media_util.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
-def query_6(cursor):
-    """
-    Listar as 5 categorias com a maior média de avaliações úteis positivas por produto.
-    """
-
-    logging.info("Executando Query 6 para listar as 5 categorias com a maior média de avaliações úteis positivas por produto.")
+def query_6(cursor, output_dir):
+    logging.info("Executando Query 6...")
 
     sql = """
-        SELECT
-            c.categorie_name,
-            AVG(r.helpful)::NUMERIC(10, 2) AS media_avaliacoes_uteis
-        FROM
-            reviews AS r
-        JOIN
-            Categorie_product AS cp ON r.asin = cp.asin
-        JOIN
-            Categories AS c ON cp.id_category = c.id_categorie
-        GROUP BY
-            c.id_categorie, c.categorie_name
-        ORDER BY
-            media_avaliacoes_uteis DESC
-        LIMIT 5;
+        SELECT c.id_categorie, c.categorie_name, AVG(r.helpful)::NUMERIC(10, 2) AS media_avaliacoes_uteis
+        FROM reviews AS r JOIN Categorie_product AS cp ON r.asin = cp.asin
+        JOIN Categories AS c ON cp.id_category = c.id_categorie
+        WHERE r.rating > 3
+        GROUP BY c.id_categorie, c.categorie_name ORDER BY media_avaliacoes_uteis DESC LIMIT 5;
     """
+
     cursor.execute(sql)
     results = cursor.fetchall()
-
-    print("\n--- Query 6: Top 5 Categorias com Maior Média de Avaliações Úteis Positivas ---\n")
+    print("\n--- Query 6: Top 5 Categorias com Maior Média de Avaliações Úteis Positivas ---")
     if results:
         df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
         print(df)
-    else:
-        print("Nenhuma categoria encontrada.")
+        file_path = os.path.join(output_dir, "q6_top5_categorias_media_util.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
+def query_7(cursor, output_dir):
+    logging.info("Executando Query 7...")
 
+    sql = """
+        WITH ranked_customers AS (
+            SELECT r.customer, p."group", COUNT(*) as comment_count,
+                   ROW_NUMBER() OVER (PARTITION BY p."group" ORDER BY COUNT(*) DESC) as rn
+            FROM reviews r JOIN products p ON r.asin = p.asin
+            GROUP BY r.customer, p."group"
+        )
+        SELECT customer, "group", comment_count 
+        FROM ranked_customers 
+        WHERE rn <= 10;
+    """
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("\n--- Query 7: Top 10 Clientes com Mais Comentários por Grupo ---")
+    if results:
+        df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
+        print(df)
+        file_path = os.path.join(output_dir, "q7_top10_clientes_por_grupo.csv")
+        df.to_csv(file_path, index=False)
+        logging.info(f"Resultado salvo em: {file_path}")
 
 def main():
     """
-    Função principal que orquestra a execução do script.
+    Função principal que define a execução do script.
     """
     parser = argparse.ArgumentParser(description="Dashboard de Consultas para o Trabalho Prático de BD")
     parser.add_argument('--db-host', required=True, help="Host do banco de dados")
@@ -235,12 +208,19 @@ def main():
     parser.add_argument('--db-name', required=True, help="Nome do banco de dados")
     parser.add_argument('--db-user', required=True, help="Usuário do banco de dados")
     parser.add_argument('--db-pass', required=True, help="Senha do banco de dados")
-    # parser.add_argument('--product-asin', help="ASIN do produto")
     parser.add_argument('--output', default='/app/out', help="Diretório para salvar os resultados em CSV")
+    parser.add_argument('--product-asin', help="ASIN do produto para consultas específicas (1, 2, 3). Se não for fornecido, usa um valor padrão.")
     
     args = parser.parse_args()
     
-    ASIN_PARA_TESTE = '0738700797'
+    ASIN_PADRAO = '0807220280'
+
+    if args.product_asin:
+        asin_para_teste = args.product_asin
+        logging.info(f"Usando ASIN fornecido pelo usuário: {asin_para_teste}")
+    else:
+        asin_para_teste = ASIN_PADRAO
+        logging.info(f"Nenhum ASIN fornecido. Usando ASIN padrão para testes: {asin_para_teste}")
 
     os.makedirs(args.output, exist_ok=True)
     
@@ -253,12 +233,14 @@ def main():
         cursor = conn.cursor()
         logging.info("Conexão bem-sucedida.")
 
-        # query_1(cursor, ASIN_PARA_TESTE)
-        # query_2(cursor, ASIN_PARA_TESTE)
-        # query_3(cursor, ASIN_PARA_TESTE)
-        # query_4(cursor)
-        # query_5(cursor)
-        query_6(cursor)
+        # executa todas as queries
+        query_1(cursor, asin_para_teste, args.output)
+        query_2(cursor, asin_para_teste, args.output)
+        query_3(cursor, asin_para_teste, args.output)
+        query_4(cursor, args.output)
+        query_5(cursor, args.output)
+        query_6(cursor, args.output)
+        query_7(cursor, args.output)
 
         cursor.close()
     except psycopg2.Error as e:
